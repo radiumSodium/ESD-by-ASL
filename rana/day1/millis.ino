9bc936596ab56396e41812e2574ccf605b19d5c7
Md. Sojibul Islam Rana <si.radiumsodium@gmail.com>
2025-01-19 12:44:41 +0600

    Blink LED with millis()

--- rana/day1/millis.ino
+++ rana/day1/millis.ino
@@ -1,32 +1,23 @@
 /*
   author : Sojibul Islam Rana
-  program: Print "Hello, Tushar Vai!!!" every second
+  program: LED blink with millis()
   date   : 16-01-2005
 */
 
+#define ledpin = 13;
 const unsigned long eventInterval = 1000;
 unsigned long previousTime = 0;
 
 void setup() {
   Serial.begin(9600); 
+  pinMode(ledpin, OUTPUT);
 }
 
 void loop() {
   unsigned long currentTime = millis();
 
   if (currentTime - previousTime >= eventInterval){
-    Serial.println("Hello, Tushar Vai!!!");
+    digitalWrite(ledpin, !digitalRead(ledpin));
     previousTime = currentTime;
   }
 }
-
-
-/* 
-  unsigned long: because the millis will count 16000 in a second
-  normal variable like int which is 2 bytes will overflow soon.
-
-  currentTime holds the current value of the counter. if the time
-  time difference is greater/equal of eventInterval which is 1000
-  or 1 second println() will print whatever inside of it. then
-  update the previousTime with currentTime 
-*/
