2ce4210a723dbceaeb9e261b6d8bce3a185f42a8
Shafi <shafideathworm7676@gmail.com>
2025-01-29 19:27:18 +0600

    day 4 assignment

---
+++ Shafi/DAY4/DAY4.ino
@@ -0,0 +1,28 @@
+#define LED 6
+#define SWITCH 2
+
+
+int prevMillis = 0;
+int currentMillis;
+
+
+void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(SWITCH, INPUT_PULLUP);
+     
+}
+
+void loop() {
+  currentMillis = millis();
+  if( digitalRead(SWITCH) == HIGH){
+    currentMillis = millis();
+    digitalWrite(LED, HIGH);
+  }
+  
+  if((currentMillis - prevMillis) == 3000){
+    digitalWrite(LED, LOW);
+    prevMillis = currentMillis;
+  }
+
+  
+}
