c59c2d9f9e3438dd17eeff4831047543f141d34d
Md. Sojibul Islam Rana <si.radiumsodium@gmail.com>
2025-01-30 11:35:24 +0600

    day5 excercise

---
+++ rana/day5/excercise1.ino
@@ -0,0 +1,16 @@
+// Exercise demo
+
+#define ledpin 13
+
+void setup()
+{
+    pinMode(ledpin, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(ledpin, HIGH);
+    delay(1000);
+    digitalWrite(ledpin, LOW);
+    delay(1000);
+}
